6e2435317ee7fde8152dcf72d25442745dd87e65
aimenaimenaimen <317449@studenti.unimore.it>
2025-06-05 12:39:18 +0200

    Created a little test suit

---
+++ gestione/.coveragerc    # Configurazione per coverage.py
@@ -0,0 +1,8 @@
+[run]
+branch = True
+source = gestione
+omit = */tests.py
+
+[report]
+show_missing = True
+skip_covered = True
--- gestione/tests.py
+++ gestione/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_404_page(self):
+        response = self.client.get('/nonexistent-page/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_coupon_page(self):
+        # Assumendo che tu abbia una pagina "about" configurata
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        # Creazione di un utente di test
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        
+        # Effettua il login
+        login = self.client.login(username='testuser', password='testpassword')
+        self.assertTrue(login)
+
+        # Verifica che l'utente sia autenticato accedendo a una pagina protetta
+        response = self.client.get('/protected-page/')  # Sostituisci con l'URL di una pagina protetta
+        self.assertEqual(response.status_code, 200)
